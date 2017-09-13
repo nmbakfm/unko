ac2c6f53f2c1f117c18ec44d64f134da250c4a54
k-masatany <masatani@fusic.co.jp>
2017-09-13 21:18:06 +0900

    Rust化完了

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    println!("
+                   人
+                  ﾉ  ﾉ
+               _／   （
+              /      ::＼
+            （         ;;）
+             \\_――￣￣ ::::＼
+           ﾉ￣          :::）
+         （            ;;;;人
+        ／￣―――――￣￣    ::::＼
+      （                :::::::）
+       ＼_______:::::;;;;;;;;;ノ
+
+     U       U  N       N  K     K  O O O O
+     U       U  N N     N  K   K    O     O
+     U       U  N   N   N  K K      O     O
+     U       U  N     N N  K   K    O     O
+      U U U U   N       N  K     K  O O O O
+");
 }
