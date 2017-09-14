ea7a1f7e5b55484f73fc7db47c24daf5eb46980f
k-masatany <masatani@fusic.co.jp>
2017-09-14 22:36:28 +0900

    add "kuso" option.

---
+++ rust/src/unko/mod.rs
@@ -0,0 +1,45 @@
+// normal print
+pub fn print(n: usize) {
+    println!("
+                   人
+                  ﾉ  ﾉ
+               _／   （
+              /      ::＼
+            （         ;;）
+             \\_――￣￣ ::::＼
+           ﾉ￣          :::）
+         （            ;;;;人
+        ／￣―――――￣￣    ::::＼
+      （                :::::::）
+       ＼_______:::::;;;;;;;;;ノ
+
+     U       U  N       N  K     K  O O O O
+     U       U  N N     N  K   K    O     O
+     U       U  N   N   N  K K      O     O
+     U       U  N     N N  K   K    O     O
+      U U U U   N       N  K     K  O O O O
+    ");
+}
+
+// print kuso
+pub fn kuso() {
+    println!("
+                   人
+                  ﾉ  ﾉ
+               _／   （
+              /      ::＼
+            （         ;;）
+             \\_――￣￣ ::::＼
+           ﾉ￣          :::）
+         （            ;;;;人
+        ／￣―――――￣￣    ::::＼
+      （                :::::::）
+       ＼_______:::::;;;;;;;;;ノ
+
+     K     K  U       U    S S S S    O O O
+     K   K    U       U  S          O       O
+     K K      U       U    S S S    O       O
+     K   K    U       U          S  O       O
+     K     K    U U U    S S S S      O O O
+    ");
+}
