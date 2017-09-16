3f022674a9530a7e02b3db0f8e65aefc37da17b0
seike460 <seike460@gmail.com>
2017-09-16 15:14:13 +0900

    :+1: unko Refactor with go

---
+++ go/src/unko.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`
+                   人
+                  ﾉ  ﾉ
+               _／   （
+              /      ::＼
+            （         ;;）
+             \_――￣￣ ::::＼
+           ﾉ￣          :::）
+         （            ;;;;人
+        ／￣―――――￣￣    ::::＼
+      （                :::::::）
+       ＼_______:::::;;;;;;;;;ノ
+
+U       U  N       N  K     K  O O O O
+U       U  N N     N  K   K    O     O
+U       U  N   N   N  K K      O     O
+U       U  N     N N  K   K    O     O
+ U U U U   N       N  K     K  O O O O`)
+}
